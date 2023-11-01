faf6f7d14362073c75cbf5e2a4666e2919e457cf
Oskar Augustynek <oskar.augustynek@gmail.com>
2023-11-01 03:41:26 +0100

    Added methods with unit tests for: -subtracting -multiplying -dividing Got rid of redundant variable in add method.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,17 +3,34 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        int sumOfAdd = add(10,10);
-        System.out.println(sumOfAdd);
+        int resultOfAdd = add(10,10);
+        System.out.println(resultOfAdd);
 
+        int resultOfSubtract = subtract(10,4);
+        System.out.println(resultOfSubtract);
+
+        int resultOfMultiply = multiply(6,4);
+        System.out.println(resultOfMultiply);
+
+        float resultOfDivide = divide(10,5);
+        System.out.println(resultOfDivide);
 
     }
 
     public static int add(int numA, int numB){
-        int result = numA + numB;
-        return result;
+        return numA + numB;
     }
 
+    public static int subtract(int numA, int numB){
+        return numA - numB;
+    }
 
+    public static int multiply (int numA, int numB){
+        return numA * numB;
+    }
+
+    public static float divide (float numA, float numB){
+        return numA / numB;
+    }
 
 }
--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -10,4 +10,22 @@ public class MainTest {
         int result = Main.add(1,2);
         Assertions.assertEquals(3,result);
     }
+
+    @Test
+    void shouldSubtractTwoFromFour() {
+        int result = Main.subtract(4,2);
+        Assertions.assertEquals(2,result);
+    }
+
+    @Test
+    void shouldMultiplyTwoWithThree() {
+        int result = Main.multiply(2,3);
+        Assertions.assertEquals(6,result);
+    }
+
+    @Test
+    void shouldDivideFourByTwo() {
+        float result = Main.divide(4,2);
+        Assertions.assertEquals(2,result);
+    }
 }
