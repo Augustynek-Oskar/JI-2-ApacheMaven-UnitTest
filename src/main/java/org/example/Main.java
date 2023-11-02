26649ec8d306ebffb7b8bf04143e0c73773bfae4
Oskar Augustynek <oskar.augustynek@gmail.com>
2023-11-02 02:55:33 +0100

    Added checkIfNumberIsEven method into main method

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,10 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println(checkIfNumberIsEven(2));
+
+        System.out.println(checkIfNumberIsEven(3));
+
 
     }
     public static boolean checkIfNumberIsEven (int number){
