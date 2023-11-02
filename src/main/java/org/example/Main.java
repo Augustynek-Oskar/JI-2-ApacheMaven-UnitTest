0225b7677046f14c13e96ec5068773e04839ebe8
Oskar Augustynek <oskar.augustynek@gmail.com>
2023-11-02 02:48:12 +0100

    Created a method that checks if number is even.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,18 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+
+
     }
+    public static boolean checkIfNumberIsEven (int number){
+        if (number %2 == 0){
+            return true;
+        }
+        else {
+            return false;
+        }
+
+    }
+
+
 }
