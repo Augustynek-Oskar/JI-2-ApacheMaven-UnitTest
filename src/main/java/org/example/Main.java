f8d731b2929e79129fabaa5355f03a4318ddf3ac
Oskar Augustynek <oskar.augustynek@gmail.com>
2023-11-02 03:12:12 +0100

    Created method for getting day of the week

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,39 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+
+
+
     }
+    public static void getDayOfTheWeek (int dayNum){
+
+        switch (dayNum){
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6, 7:
+                System.out.println("Weekend");
+                break;
+            default:
+                System.out.println("There is no such day!");
+                break;
+
+        }
+
+    }
+
+
+
 }
