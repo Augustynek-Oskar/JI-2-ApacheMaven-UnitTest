7859732405ae4db04f4a54156119d0f791c99c59
Oskar Augustynek <oskar.augustynek@gmail.com>
2023-11-02 02:38:27 +0100

    Created a method that checks if user is adult. Created a unit test for that method.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,20 @@
 package org.example;
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+
+    boolean isAdult = checkIfUserIsAdult(17);
+        System.out.println(isAdult);
+
+
     }
+
+    public static boolean checkIfUserIsAdult (int age) {
+        if (age >= 18){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
 }
