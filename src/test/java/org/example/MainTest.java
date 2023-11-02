0f761b5fa8954b2ee981561eb34b715d85d611bf
Oskar Augustynek <oskar.augustynek@gmail.com>
2023-11-02 02:52:13 +0100

    Added unit test for method checkIfNumberIsEven

---
+++ src/test/java/org/example/MainTest.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void shouldCheckIfNumberIsEven() {
+
+        boolean result = Main.checkIfNumberIsEven(10);
+        assertTrue(result);
+
+        boolean result2 = Main.checkIfNumberIsEven(9);
+        assertFalse(result2);
+
+    }
+}
