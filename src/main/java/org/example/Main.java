37b77ab2d008e85a0241beb708d9f34dccde4062
Oskar Augustynek <oskar.augustynek@gmail.com>
2023-11-03 16:49:55 +0100

    Deleted main method.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,5 @@
 package org.example;
 public class Main {
-    public static void main(String[] args) {
-
-    boolean isAdult = checkIfUserIsAdult(17);
-        System.out.println(isAdult);
-
-
-    }
-
     public static boolean checkIfUserIsAdult (int age) {
         if (age >= 18){
             return true;
