2a6d5771e3d937928b7d108c7f213dcdded39ec4
Oskar Augustynek <oskar.augustynek@gmail.com>
2023-11-06 03:29:03 +0100

    Deleted main method.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,21 +1,6 @@
 package org.example;
 
 public class Main {
-    public static void main(String[] args) {
-
-        int resultOfAdd = add(10,10);
-        System.out.println(resultOfAdd);
-
-        int resultOfSubtract = subtract(10,4);
-        System.out.println(resultOfSubtract);
-
-        int resultOfMultiply = multiply(6,4);
-        System.out.println(resultOfMultiply);
-
-        float resultOfDivide = divide(10,5);
-        System.out.println(resultOfDivide);
-
-    }
 
     public static int add(int numA, int numB){
         return numA + numB;
